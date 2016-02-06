9157fd11164d0c0615b20af0db63471f6707f933
Odell Dotson <odelldotson@gmail.com>
2016-02-06 15:20:05 -0500

    Added feature 5, combined Ethan's features into the main code.

--- proj3/generateFeatures.py
+++ proj3/generateFeatures.py
@@ -12,52 +12,151 @@ def classify():
     inDataFile = open(inFileName, 'r')
     outDataFile = open(outFileName, 'w')
 
-def bottomLeftCornerControl():
-    """
-    Which player has control over the bottom left corner. ??
-    """
+# uses the bottom left space as the feature, this is the first required feature
+def getFeature1(board):
+    return board[0]
 
-def moreInCenerOfBoard():
-    """
-    Which player has more in the collumns that are not in the left or right extremes.
-    """
+# uses which player has more non edge pieces as the feature, this is the second required feature
+def getFeature2(board):
+    count = 0
+    for x in xrange(6,36):
+        if board[x] == 1:
+            count += 1
+        elif board[x] == 2:
+            count -= 1
+    if count > 0:
+        return 1
+    elif count < 0:
+        return 2
+    else:
+        return 0
 
-def centerControl():
-    """center disc: discs in the center column owned by each player
-    centerControl is the difference of the number of center discs between the first player and the second player.
-    Type: Numeric"""
-    pass
+# uses a weight based on the pieces distance from the center to calculate each players value and uses the player with the highest value as the feature
+def getFeature3(board):
+    count = 0
+    for x in xrange(0,42):
+        if board[x] != 0:
+            if x <= 5 or x >= 36:
+                count = count + 2 * (1.5 - board[x])
+            elif x <= 11 or x >= 30:
+                count = count + 4 * (1.5 - board[x])
+            elif x <= 17 or x >= 24:
+                count = count + 8 * (1.5 - board[x])
+            else:
+                count = count + 16 * (1.5 - board[x])
+    if count > 0:
+        return 1
+    elif count < 0:
+        return 2
+    else:
+        return 0
 
-def openRun():
-    """Open discs : Pairs or triplets of connected discs that a player can still build off of. These sets of discs can be horizontal, vertical, or diagonal.
-    OpenRun: the difference of the number of open discs between the first player and the second player.
-    Type: Numeric"""
+# uses a weight based on the pieces distance from the center and whose piece it is to calculate a value for the board state and returns that value
+def getFeature4(board):
+    count = 0
+    for x in xrange(0,42):
+        if board[x] != 0:
+            if x <= 5 or x >= 36:
+                count = count + 2 * (1.5 - board[x])
+            elif x <= 11 or x >= 30:
+                count = count + 4 * (1.5 - board[x])
+            elif x <= 17 or x >= 24:
+                count = count + 8 * (1.5 - board[x])
+            else:
+                count = count + 16 * (1.5 - board[x])
+    return count
 
 
-def openFirstLevel():
-    """Open bottom discs: the number of open discs on the bottom row in the connect-4 board.
-    OpenFirstLevel: the number of open bottom discs
-    Type:Numeric"""
+# Checks who has the greater number of top-of-column control pieces.
+def getFeature5(): # Dear future employers: I know this code is shit I have a deadline to meet I'm sorry
+    redTops = 0
+    yellowTops = 0
+    toAdd = 0
+    x = 0
+
+    while (x<6 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+            toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 6
+
+    while (x<12 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 12
+
+    while (x<18 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 18
+
+    while (x<24 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 24
+
+    while (x<30 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 30
+
+    while (x<36 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
+
+    toAdd = 0
+    x = 36
+
+    while (x<42 and toAdd == 0):
+        if board[x] == 0 and x != 0:
+                toAdd = board[x-1]
+        x+=1
+    if toAdd == 1:
+        redTops += 1
+    elif toAdd == 1:
+        yellowTops +=1
 
 
-def moreInCenter():
-    """center disc: discs not located in the outermost columns (The first and last column)
-    MoreInCenter: the player who has more center discs, otherwise 0 which indicates a tie.
-    Type: Nominal"""
 
 
-def scoreOccupied():
-    """
-    ScoreBoard: The number of possible connect-4 paths still available to each player.
-    ScoreOccupied: the difference between scoreBoard of the first player and  the second player.
-    Type: Numeric
-    """
-
-
-def firstRun():
-    """
-    The player who makes the next move after given board.
-    Type: Nominal
-    """
-
+getFeature5()
 
