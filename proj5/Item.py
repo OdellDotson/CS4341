a2d55138a2fc2d0b52fdb3aeffdeab932d439343
Ethanprihar <ethanprihar@gmail.com>
2016-02-19 13:30:34 -0500

    fixed mutual inclusive structure
    now the partnerItems variable is a list of every item that has a mutual
    inclusion with the item. partnerBags is now a list of lists of the
    corresponding mutual bags.

--- proj5/Item.py
+++ proj5/Item.py
@@ -10,14 +10,8 @@ class Item:
 		self.allowedBags = [] # list of bags that this item can be in
 		self.mustBeWith = [] # list of items that must be in the same bag as this item
 		self.cantBeWith = [] # list of items that cannot be in the same bad as this item
-
-		# @TODO: This needs a big restructure so it takes into account the fact that a single item can have multiple partners each with exclusive bag pairings.
-		self.partnerItem = [] # Mutually exclusive item. Empty if this item does not have a mutually inclusive binary constraint.
-
-		# How it used to look, for your reference:
-		#Item("noItem", 0) # mutually exclusive item
-
-		self.partnerBags = [] # the bags that the mutually exclusive item can be in
+		self.partnerItems = [] # Mutually exclusive items. Empty if this item does not have a mutually inclusive binary constraint.
+		self.partnerBags = [] # the bags that the mutually exclusive items can be in, all arrays of 2 bags
 
 	# This function will return true if an item is allowed in a bag and false if it is not.
 	def canBeIn(self, bag):
@@ -31,16 +25,19 @@ class Item:
 				return False
 		if bag.totalWeight + self.weight > 100: # if the bag will be over capacity if this item is placed in it
 			return False
-		if bag.numItems = bag.maxItems
+		if bag.numItems == bag.maxItems:
 			return False
-		if self.partnerItem.inBag in self.partnerBags and self.partnerItem.inBag.equals(bag): # if this item's mutual inclusive partner item is in one of the mutual bags and that bag is the bag
-			return False
-		if self.partnerItem.inBag in self.partnerBags and bag not in self.partnerBags: # if this item's mutual inclusive partner item is in one of the mutual bags and the bag is not a mutual bag
-			return False
-		if self.partnerItem.inBag not in self.partnerBags:
-			for partnerBag in self.partnerBags:
-				if partnerBag.equals(bag):
-					return False
+		# @TODO
+		# read this so that you understand what has been changed
+		for i in xrange(0, len(partnerItems) - 1):
+			if self.partnerItems[i].inBag in partnerBags[i] and self.partnerItems[i].inBag.equals(bag): # if this item's mutual inclusive partner item is in one of the mutual bags and that bag is the bag
+				return False
+			if self.partnerItems[i].inBag in self.partnerBags[i] and bag not in self.partnerBags[i]: # if this item's mutual inclusive partner item is in one of the mutual bags and the bag is not a mutual bag
+				return False
+			if self.partnerItems[i].inBag not in self.partnerBags[i]: # if the partner item is not in a partner bag
+				for partnerBag in self.partnerBags[i]:
+					if partnerBag.equals(bag):
+						return False
 		return True
 
 	def equals(self, other):
