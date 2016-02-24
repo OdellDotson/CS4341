3ee954597bf2353ab89bcc18107b73ab2b6292fa
Odell Dotson <odelldotson@gmail.com>
2016-02-24 16:24:05 -0500

    Adgfhm

--- proj6/Node.py
+++ proj6/Node.py
@@ -4,51 +4,51 @@ import random
 
 class Node:
 
-	def __init__(self, name, probabiltyList):
-		self.name = name
-		self.parents = []
-		self.probabilty = list(probabiltyList)
-		self.truth = -1
+    def __init__(self, name, probabiltyList):
+        self.name = name
+        self.parents = []
+        self.probability = list(probabiltyList)
+        self.truth = -1
 
-	def addParents(self, parentList):
-		self.parents = list(reversed(parentList))
+    def addParents(self, parentList):
+        self.parents = list(reversed(parentList))
 
-	def getTruthLW(self):
-		state = ""
-		for parent in self.parents:
-			state = state + str(parent.getTruthLW())
-		if state == "":
-			pos = 0
-		else:
-			pos = int(state, 2)
-		if self.truth == 1: #In the case that we are forcing self to be true:
+    def getTruthLW(self):
+        state = ""
+        for parent in self.parents:
+            state += str(parent.getTruthLW())
+        if state == "":
+            pos = 0
+        else:
+            pos = int(state, 2)
+        if self.truth == 1: #In the case that we are forcing self to be true:
+            weight = self.probability
+        if random.random() < self.probability[pos]:
+            truth = 1
+        else:
+            truth = 0
 
-		if(random.random() < self.probabilty[pos]):
-			truth = 1
-		else:
-			truth = 0
 
+        return (weight, truth)
 
-		return (weight, truth)
-
-	def getTruthRS(self):		
-		state = ""
-		for parent in self.parents:
-			truth = parent.getTruthRS()
-			if truth = -1:
-				return -1
-			state = state + str(truth)
-		if state == "":
-			pos = 0
-		else:
-			pos = int(state, 2)
-		if(random.random() < self.probabilty[pos]):
-			sample = 1
-		else:
-			sample = 0
-		if self.truth == -1
-			return sample
-		elif sample == self.truth:
-			return sample
-		else:
-			return -1
+    def getTruthRS(self):
+        state = ""
+        for parent in self.parents:
+            truth = parent.getTruthRS()
+            if truth == -1:
+                return -1
+            state += str(truth)
+        if state == "":
+            pos = 0
+        else:
+            pos = int(state, 2)
+        if random.random() < self.probability[pos]:
+            sample = 1
+        else:
+            sample = 0
+        if self.truth == -1:
+            return sample
+        elif sample == self.truth:
+            return sample
+        else:
+            return -1
