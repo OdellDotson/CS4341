308e97f6071322b1d162cdab740d6cef54edab1a
Ethanprihar <ethanprihar@gmail.com>
2016-02-24 14:56:39 -0500

    made the node class

---
+++ proj6/Node.py
@@ -0,0 +1,24 @@
+#Odell Dotson and Ethan Prihar, ocdotson@wpi.edu ebprihar@wpi.edu, Intro to AI Project 5
+
+import random
+
+class Node:
+
+	def __init__(self, name, parentList, probabiltyList):
+		self.name = name
+		self.parents = list(reversed(parentList))
+		self.probabilty = list(truthList)
+		self.truth = -1
+
+	def getTruth(self):
+		state = ""
+		for parent in self.parents:
+			state = state + str(parent.getTruth())
+		if (self.truth == -1):
+			pos = int(state, 2)
+			if(random.random() < probabilty[pos]):
+				return 1
+			else:
+				return 0
+		else:
+			return self.truth
